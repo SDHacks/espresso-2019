e223c723470aba301674273e8d88aa010076c538
Lisa Luo <llluo@ucsd.edu>
2019-10-17 17:25:44 -0700

    added no registration required!

--- js/company-table.js
+++ js/company-table.js
@@ -44,29 +44,35 @@ $(function(){
 
       console.log(industries, positions, workAuths)
       
-      industries.forEach(function(industry) {
-        if (Object.keys(uniqueIndustries).indexOf(industry) === -1) {
-          uniqueIndustries[industry] = 1;
-        } else {
-          uniqueIndustries[industry]++;
-        }
-      });
+      if (industries[0] != '') {
+        industries.forEach(function(industry) {
+          if (Object.keys(uniqueIndustries).indexOf(industry) === -1) {
+            uniqueIndustries[industry] = 1;
+          } else {
+            uniqueIndustries[industry]++;
+          }
+        });
+      }
 
-      positions.forEach(function(position) {
-        if (Object.keys(uniquePositions).indexOf(position) === -1) {
-          uniquePositions[position] = 1;
-        } else {
-          uniquePositions[position]++;
-        }
-      });
+      if (positions[0] != '') {
+        positions.forEach(function(position) {
+          if (Object.keys(uniquePositions).indexOf(position) === -1) {
+            uniquePositions[position] = 1;
+          } else {
+            uniquePositions[position]++;
+          }
+        });
+      }   
 
-      workAuths.forEach(function(workAuth) {
-        if (Object.keys(uniqueWorkAuths).indexOf(workAuth) === -1) {
-          uniqueWorkAuths[workAuth] = 1;
-        } else {
-          uniqueWorkAuths[workAuth]++;
-        }
-      });
+      if (workAuths[0] != '') {
+        workAuths.forEach(function(workAuth) {
+          if (Object.keys(uniqueWorkAuths).indexOf(workAuth) === -1) {
+            uniqueWorkAuths[workAuth] = 1;
+          } else {
+            uniqueWorkAuths[workAuth]++;
+          }
+        });
+      }
     });
     
     Object.keys(uniqueIndustries).forEach(function(industry) {
@@ -120,7 +126,7 @@ $(function(){
   function setCompanyFilter() {
     $('.company-table__search').bind('input', function() {
       var filter = $(this).val();
-      activeFilters.name = filter;
+      activeFilters.name = filter.toLowerCase();
       onFilterChanged();
     });
   }
@@ -161,7 +167,7 @@ $(function(){
 
   function onFilterChanged() {
     $('#companies .company').each(function() {
-      var name = $('.company__name', this).text();
+      var name = $('.company__name', this).text().toLowerCase();
       var industry = $('.company__industry span', this).text();
       var position = $('.company__position span', this).text();
       var workAuth = $('.company__auth span', this).text();
